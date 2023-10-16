1df9f84cafae607e602bcacf99e20109ef85bace
Mihir Agarwal <mihiragarwal1710@gmail.com>
2023-10-16 05:32:39 +0530

    Changed the typography for the references

--- streamlit_demo.py
+++ streamlit_demo.py
@@ -403,93 +403,112 @@ Conformal Prediction doesn't make strong assumptions about the data distribution
     st.markdown("## **References:**")
     st.markdown(
     '''
-    <div style="font-family: Times New Roman; font-size: 16px; color: grey;">
-        [1] A. Angelopoulos, "A Gentle Introduction to Conformal Prediction and Distribution-Free Uncertainty Quantification," [Online]. Available: <a style="color: grey;" href='https://people.eecs.berkeley.edu/~angelopoulos/publications/downloads/gentle_intro_conformal_dfuq.pdf'>https://people.eecs.berkeley.edu/~angelopoulos/publications/downloads/gentle_intro_conformal_dfuq.pdf</a>.
+    <div style="font-family: 'Computer Modern'; font-size: 13px; color: gray; padding: 10px 0; ">
+        <strong>[1] A. Angelopoulos</strong>, "A Gentle Introduction to Conformal Prediction and Distribution-Free Uncertainty Quantification," [Online]. Available: 
+        <a style="color: #A7AAA4; text-decoration: none;" href='https://people.eecs.berkeley.edu/~angelopoulos/publications/downloads/gentle_intro_conformal_dfuq.pdf' onmouseover="this.style.color='#0056b3'" onmouseout="this.style.color='#007BFF'">https://people.eecs.berkeley.edu/~angelopoulos/publications/downloads/gentle_intro_conformal_dfuq.pdf</a>.
     </div>
     ''',
     unsafe_allow_html=True,
 )
 
+
     st.markdown(
     '''
-    <div style="font-family: Times New Roman; font-size: 16px; color: grey;">
-        [2] I. A. Auzina, L. Bereska, A. Timans, and E. Nalisnick, "Tutorial on Conformal Predictions," in UvA DL Notebooks, [Online]. Available: <a style="color: grey;" href='https://uvadlc-notebooks.readthedocs.io/en/latest/tutorial_notebooks/DL2/Bayesian_Neural_Networks/dl2_bnn_tut2_student_with_answers.html#Conformal-prediction'>https://uvadlc-notebooks.readthedocs.io/en/latest/tutorial_notebooks/DL2/Bayesian_Neural_Networks/dl2_bnn_tut2_student_with_answers.html#Conformal-prediction</a>.
-    </div>
-    ''',
+<div style="font-family: 'Computer Modern'; font-size: 13px; color: gray; padding: 10px 0; ">
+    <strong>[2] I. A. Auzina, L. Bereska, A. Timans, and E. Nalisnick</strong>, "Tutorial on Conformal Predictions," [Online]. Available: 
+    <a style="color: #A7AAA4; text-decoration: none;" href='https://uvadlc-notebooks.readthedocs.io/en/latest/tutorial_notebooks/DL2/Bayesian_Neural_Networks/dl2_bnn_tut2_student_with_answers.html#Conformal-prediction' onmouseover="this.style.color='#0056b3'" onmouseout="this.style.color='#007BFF'">https://uvadlc-notebooks.readthedocs.io/en/latest/tutorial_notebooks/DL2/Bayesian_Neural_Networks/dl2_bnn_tut2_student_with_answers.html#Conformal-prediction</a>.
+</div>
+''',
     unsafe_allow_html=True,
 )
 
     st.markdown(
     '''
-    <div style="font-family: Times New Roman; font-size: 16px; color: grey;">
-        [3] G. Shafer and V. Vovk, "A Tutorial on Conformal Predictions," Journal of Machine Learning Research, vol. 9, no. 12, pp. 371-421, 2008. [Online]. Available: <a style="color: grey;" href='https://www.jmlr.org/papers/volume9/shafer08a/shafer08a.pdf'>https://www.jmlr.org/papers/volume9/shafer08a/shafer08a.pdf</a>.
+    <div style="font-family: 'Computer Modern'; font-size: 13px; color: gray; padding: 10px 0; ">
+        <strong>[3] G. Shafer and V. Vovk</strong>, "A Tutorial on Conformal Predictions," Journal of Machine Learning Research, vol. 9, no. 12, pp. 371-421, 2008. [Online]. Available: 
+        <a style="color: #A7AAA4; text-decoration: none;" href='https://www.jmlr.org/papers/volume9/shafer08a/shafer08a.pdf' onmouseover="this.style.color='#0056b3'" onmouseout="this.style.color='#007BFF'">https://www.jmlr.org/papers/volume9/shafer08a/shafer08a.pdf</a>.
     </div>
     ''',
     unsafe_allow_html=True,
 )
 
+    # Snippet 4
     st.markdown(
-    '''
-    <div style="font-family: Times New Roman; font-size: 16px; color: grey;">
-        [4] Howard, Jeremy and others, "fastai: A Layered API for Deep Learning", GitHub, [Online]. Available: <a style="color: grey;" href=https://github.com/fastai/fastai>https://github.com/fastai/fastai</a>.
-    </div>
-    ''',
-    unsafe_allow_html=True,
-)
-    
-    st.markdown(
-    '''
-    <div style="font-family: Times New Roman; font-size: 16px; color: grey;">
-        [5] Neil D. Lawrence, "Deep Gaussian Processes", [Online]. Available: <a style="color: grey;" href=http://inverseprobability.com/talks/notes/deep-gaussian-processes.html>http://inverseprobability.com/talks/notes/deep-gaussian-processes.html</a>.
-    </div>
-    ''',
-    unsafe_allow_html=True,
-)
-    
-    st.markdown(
-    '''
-    <div style="font-family: Times New Roman; font-size: 16px; color: grey;">
-        [6] "Resnet Example for Green Apples and Oranges", [Online]. Available: <a style="color: grey;" href=https://www.kaggle.com/mihiragarwal1710/is-it-a-bird-creating-a-model-from-your-ow-920b0c>https://www.kaggle.com/mihiragarwal1710/is-it-a-bird-creating-a-model-from-your-ow-920b0c</a>.
-    </div>
-    ''',
-    unsafe_allow_html=True,
+        '''
+        <div style="font-family: 'Computer Modern'; font-size: 13px; color: gray; padding: 10px 0; ">
+            <strong>[4] Howard, Jeremy and others</strong>, "fastai: A Layered API for Deep Learning", GitHub, [Online]. Available: 
+            <a style="color: #A7AAA4; text-decoration: none;" href='https://github.com/fastai/fastai' onmouseover="this.style.color='#0056b3'" onmouseout="this.style.color='#007BFF'">https://github.com/fastai/fastai</a>.
+        </div>
+        ''',
+        unsafe_allow_html=True,
     )
 
+    # Snippet 5
     st.markdown(
-    '''
-    <div style="font-family: Times New Roman; font-size: 16px; color: grey;">
-        [7] Valery Manokhin, "Awesome Conformal Prediction", GitHub, [Online]. Available: <a style="color: grey;" href=https://github.com/valeman/awesome-conformal-prediction>https://github.com/valeman/awesome-conformal-prediction</a>.
-    </div>
-    ''',
-    unsafe_allow_html=True,
+        '''
+        <div style="font-family: 'Computer Modern'; font-size: 13px; color: gray; padding: 10px 0; ">
+            <strong>[5] Neil D. Lawrence</strong>, "Deep Gaussian Processes", [Online]. Available: 
+            <a style="color: #A7AAA4; text-decoration: none;" href='http://inverseprobability.com/talks/notes/deep-gaussian-processes.html' onmouseover="this.style.color='#0056b3'" onmouseout="this.style.color='#007BFF'">http://inverseprobability.com/talks/notes/deep-gaussian-processes.html</a>.
+        </div>
+        ''',
+        unsafe_allow_html=True,
+    )
+
+    # Snippet 6
+    st.markdown(
+        '''
+        <div style="font-family: 'Computer Modern'; font-size: 13px; color: gray; padding: 10px 0; ">
+            <strong>[6] "Resnet Example for Green Apples and Oranges"</strong>, [Online]. Available: 
+            <a style="color: #A7AAA4; text-decoration: none;" href='https://www.kaggle.com/mihiragarwal1710/is-it-a-bird-creating-a-model-from-your-ow-920b0c' onmouseover="this.style.color='#0056b3'" onmouseout="this.style.color='#007BFF'">https://www.kaggle.com/mihiragarwal1710/is-it-a-bird-creating-a-model-from-your-ow-920b0c</a>.
+        </div>
+        ''',
+        unsafe_allow_html=True,
+    )
+
+    # Snippet 7
+    st.markdown(
+        '''
+        <div style="font-family: 'Computer Modern'; font-size: 13px; color: gray; padding: 10px 0; ">
+            <strong>[7] Valery Manokhin</strong>, "Awesome Conformal Prediction", GitHub, [Online]. Available: 
+            <a style="color: #A7AAA4; text-decoration: none;" href='https://github.com/valeman/awesome-conformal-prediction' onmouseover="this.style.color='#0056b3'" onmouseout="this.style.color='#007BFF'">https://github.com/valeman/awesome-conformal-prediction</a>.
+        </div>
+        ''',
+        unsafe_allow_html=True,
     )
     
+    # Snippet 8
     st.markdown(
-    '''
-    <div style="font-family: Times New Roman; font-size: 16px; color: grey;">
-        [8] Tennis Ball Image, [Online]. Available: <a style="color: grey;" href=https://www.tenniswarehouse.com.au/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/g/r/green_2balls.1601957941.jpg>https://www.tenniswarehouse.com.au/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/g/r/green_2balls.1601957941.jpg</a>.
-    </div>
-    ''',
-    unsafe_allow_html=True,
+        '''
+        <div style="font-family: 'Computer Modern'; font-size: 13px; color: gray; padding: 10px 0; ">
+            <strong>[8] Tennis Ball Image</strong>, [Online]. Available: 
+            <a style="color: #A7AAA4; text-decoration: none;" href='https://www.tenniswarehouse.com.au/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/g/r/green_2balls.1601957941.jpg' onmouseover="this.style.color='#0056b3'" onmouseout="this.style.color='#007BFF'">https://www.tenniswarehouse.com.au/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/g/r/green_2balls.1601957941.jpg</a>.
+        </div>
+        ''',
+        unsafe_allow_html=True,
     )
-    
+
+    # Snippet 9
     st.markdown(
-    '''
-    <div style="font-family: Times New Roman; font-size: 16px; color: grey;">
-        [9] Green Colored Orange Image, [Online]. Available: <a style="color: grey;" href=https://thumbs.dreamstime.com/z/green-orange-fruits-over-white-background-green-orange-fruits-127174164.jpg>https://thumbs.dreamstime.com/z/green-orange-fruits-over-white-background-green-orange-fruits-127174164.jpg</a>.
-    </div>
-    ''',
-    unsafe_allow_html=True,
+        '''
+        <div style="font-family: 'Computer Modern'; font-size: 13px; color: gray; padding: 10px 0; ">
+            <strong>[9] Green Colored Orange Image</strong>, [Online]. Available: 
+            <a style="color: #A7AAA4; text-decoration: none;" href='https://thumbs.dreamstime.com/z/green-orange-fruits-over-white-background-green-orange-fruits-127174164.jpg' onmouseover="this.style.color='#0056b3'" onmouseout="this.style.color='#007BFF'">https://thumbs.dreamstime.com/z/green-orange-fruits-over-white-background-green-orange-fruits-127174164.jpg</a>.
+        </div>
+        ''',
+        unsafe_allow_html=True,
     )
-    
+
+    # Snippet 10
     st.markdown(
-    '''
-    <div style="font-family: Times New Roman; font-size: 16px; color: grey;">
-        [10] Frog Image, [Online]. Available: <a style="color: grey;" href=https://wallup.net/wp-content/uploads/2016/01/214917-animals-wildlife-nature-frog-amphibian-Red-Eyed_Tree_Frogs.jpg>https://wallup.net/wp-content/uploads/2016/01/214917-animals-wildlife-nature-frog-amphibian-Red-Eyed_Tree_Frogs.jpg</a>.
-    </div>
-    ''',
-    unsafe_allow_html=True,
+        '''
+        <div style="font-family: 'Computer Modern'; font-size: 13px; color: gray; padding: 10px 0; ">
+            <strong>[10] Frog Image</strong>, [Online]. Available: 
+            <a style="color: #A7AAA4; text-decoration: none;" href='https://wallup.net/wp-content/uploads/2016/01/214917-animals-wildlife-nature-frog-amphibian-Red-Eyed_Tree_Frogs.jpg' onmouseover="this.style.color='#0056b3'" onmouseout="this.style.color='#007BFF'">https://wallup.net/wp-content/uploads/2016/01/214917-animals-wildlife-nature-frog-amphibian-Red-Eyed_Tree_Frogs.jpg</a>.
+        </div>
+        ''',
+        unsafe_allow_html=True,
     )
+
     
 if __name__ == "__main__":
     main()
